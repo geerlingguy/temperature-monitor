0aa6df5ab2f9afb8fc2c63ecfa5d949b665df74a
Jeff Geerling <geerlingguy@mac.com>
2015-02-02 14:09:38 -0600

    Add MySQL dependency.

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'test',
+    password: 'test'
+  });
+
   res.render('index', { title: 'Temperature Monitor' });
 });
 
